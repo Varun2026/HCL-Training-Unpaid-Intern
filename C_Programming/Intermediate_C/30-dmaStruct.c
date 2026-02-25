2af88322dd0221cdc2417829d60352399abb2a8b
VARUN KUMAR V <132676805+Varun2026@users.noreply.github.com>
2026-02-25 11:02:58 +0530

    Refactor object management with add, delete, display

--- C_Programming/Intermediate_C/30-dmaStruct.c
+++ C_Programming/Intermediate_C/30-dmaStruct.c
@@ -3,22 +3,72 @@
 #include<string.h>
 
 typedef struct{
+    int id;
     char name[20];
     int len;
 }Object;
+
+
 
 void add(Object *o, int *cnt, int size){
     if(*cnt >=size){
         printf("array of structure is full");
         return;
     }
-    while(*cnt <)
+    while(*cnt < size){
+        printf("Enter the object id: \n");
+        scanf("%d", &o[*cnt].id);
+
+        printf("Enter the object name: \n");
+        scanf("%s", o[*cnt].name);
+
+        printf("Enter the length of the obejct: \n");
+        scanf("%d", &o[*cnt].len);
+
+        (*cnt)++;   
+    }
+}
+
+void delete(Object *o, int *cnt, int id){
+    int index =-1;
+    for(int i=0; i<*cnt; i++){
+        if(o[i].id == id){
+            index = i;
+            break;
+        }
+    }
+    if(index == -1){
+        printf("No object found to delete\n");
+        return;
+    }
+
+    for(int i= index; i<*cnt -1; i++){
+        o[i] = o[i+1];
+    }
+    (*cnt)--;
+    printf("Item Deleted.\n");
+}
+
+void display(Object *arr, int *cnt){
+    if(*cnt ==0){
+        printf("No items to display");
+        return;
+    }
+    for(int i=0; i<*cnt; i++){
+        printf("Detail no. %d\n", (i+1));
+        printf("%d \n", arr[i].id);
+        printf("%s \n", arr[i].name);
+        printf("%d \n", arr[i].len);
+    }
 }
 
 int main(){
     int size;
     int choice;
     int count =0;
+    int id = 0;
+
+    // Object o[30];
     printf("Enter the size of the object: \n");
     scanf("%d", &size);
 
@@ -33,6 +83,12 @@ int main(){
         break;
 
         case 2:
-        delete()
+        printf("Enter the id to be deleted: \n");
+        scanf("%d", &id);
+        delete(d1, &count, id);
+
+        case 3:
+        display(d1, &count);
     }
+    return 0;
 }
